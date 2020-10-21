6b99be1f0f4cef66de800074e90c53e84233896d
Ashish Goolla <ashishgoolla@ufl.edu>
2020-10-20 21:53:26 -0400

    Added config file

---
+++ src/databaseconnection/config.js
@@ -0,0 +1,6 @@
+export default {
+    db: {
+      uri:
+        "mongodb+srv://ashishgoolla:Ramukhsihsa123!@tosscluster.oq6ub.mongodb.net/<dbname>?retryWrites=true&w=majority", //place the URI of your mongo database here.
+    },
+  };
