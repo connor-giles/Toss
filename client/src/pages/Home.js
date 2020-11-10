f2bc364bec6df08c0ee5782f41211fe1cd0bda12
Anna Le <annale1501@gmail.com>
2020-11-10 01:40:10 -0500

    displayed tosses

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import './Home.css'
 
 function Home() {
+    const [phaseOne, setPhaseOne] = useState([]);
+    const [phaseTwo, setPhaseTwo] = useState([]);
+    const [phaseZero, setPhaseZero] = useState([]);
 
+    useEffect(() => {
+        axios.get('http://localhost:3000/toss/3Phase1Tosses')
+        .then((response) => setPhaseZero(response.data.data.tosses))
+        .catch((error) => console.error(error))
+    }, []);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/toss/3Phase1Tosses')
+        .then((response) => setPhaseOne(response.data.data.tosses))
+        .catch((error) => console.error(error))
+    }, []);
+    
+    useEffect(() => {
+        axios.get('http://localhost:3000/toss/3Phase2Tosses')
+        .then((response) => setPhaseTwo(response.data.data.tosses))
+        .catch((error) => console.error(error))
+    }, []);
+    
     return (
         <div className="homePage">
             <div className="landing">
@@ -10,13 +32,34 @@ function Home() {
                 <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                     Sed tincidunt non enim ut pretium. Morbi accumsan dignissim gravida. </p>
             </div>
-            <div className="mainInfo">
-                 <h2>Testing</h2>
-                 <h2>Testing</h2>
-                 <h2>Testing</h2>
-                 <h2>Testing</h2>
-                 <h2>Testing</h2>
+
+            <div className="tossContainer">
+            <div className="tossZero">
+                    <div>
+                        <h1 className="colTitle">Phase Zero</h1>
+                        <h2 className="prompts">Prompts</h2>
+                        {phaseZero.map(toss => 
+                        <div className="info">{toss.prompt}</div>)}
+                    </div>
+                </div>
+                <div className="tossOne">
+                    <div>
+                        <h1 className="colTitle">Phase One</h1>
+                        <h2 className="prompts">Prompts</h2>
+                        {phaseOne.map(toss => 
+                        <div className="info">{toss.prompt}</div>)}
+                    </div>
+                </div>
+                <div className="tossTwo">
+                    <div>
+                         <h1 className="colTitle">Phase Two</h1>
+                         <h2 className="prompts">Prompts</h2>
+                        {phaseTwo.map(toss => 
+                        <div className="info">{toss.prompt}</div>)}
+                    </div>
+                </div>
             </div>
+        
         </div>
     )
 }
--- server/express.js
+++ server/express.js
@@ -8,6 +8,7 @@ const path = require('path'),
   tossRouter = require('./routes/tossRouter'),
   userRouter = require('./routes/userRouter');
 
+  var cors = require('cors')
 module.exports.init = () => {
   /* connect to database
         - reference README for db uri
@@ -28,6 +29,7 @@ module.exports.init = () => {
   // enable request logging for development debugging
   app.use(morgan('dev'));
 
+  app.use(cors());
   // body parsing middleware
   app.use(bodyParser.json());
 
