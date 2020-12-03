3534b3c55959dc036687e94b8462ef8e7f99ccc8
colmepao <paaolo1022@gmail.com>
2020-12-03 14:40:41 -0500

    Functionality for giving a user a Toss from today's Toss's

--- client/src/pages/Tosses/js/Phase1.js
+++ client/src/pages/Tosses/js/Phase1.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import '../css/Phase1.css'
+import '../css/Phase1.css';
 import axios from 'axios';
 
 export default class SubmitResponse extends Component {
   constructor(props) {
     super(props);
 
-    this.onInputUser = this.onInputUser.bind(this);
     this.onInputSource = this.onInputSource.bind(this);
     this.onInputComment = this.onInputComment.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -18,10 +17,6 @@ export default class SubmitResponse extends Component {
     };
   }
 
-  onInputUser(e) {
-    this.setState({ userID: e.target.value });
-  }
-
   onInputSource(e) {
     this.setState({ source: e.target.value });
   }
@@ -29,14 +24,13 @@ export default class SubmitResponse extends Component {
   onInputComment(e) {
     this.setState({ comment: e.target.value });
     var totalCount = e.target.value;
-    this.setState({ count: totalCount.length})
+    this.setState({ count: totalCount.length });
   }
 
   onSubmit(e) {
     e.preventDefault();
 
     const tossResponse = {
-      userID: this.state.userID,
       source: this.state.source,
       comment: this.state.comment,
     };
@@ -50,25 +44,17 @@ export default class SubmitResponse extends Component {
         console.log(error);
       });
 
-    this.setState({ userID: '', source: '', comment: ''});
+    this.setState({ userID: '', source: '', comment: '' });
   }
 
   render() {
     return (
       <div className="page">
-        <form className="wrapper"
-          onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <label className="label">Add User Name</label>
-            <input className="user-input"
-              type="text"
-              value={this.state.userID}
-              onChange={this.onInputUser}
-            />
-          </div>
+        <form className="wrapper" onSubmit={this.onSubmit}>
           <div className="form-group">
             <label className="label"> Input Comment </label>
-            <textarea className="comment-input"
+            <textarea
+              className="comment-input"
               type="text"
               value={this.state.source}
               onChange={this.onInputSource}
@@ -77,17 +63,15 @@ export default class SubmitResponse extends Component {
           </div>
           <div className="form-group">
             <label className="label"> Input Source </label>
-            <input className="source-input"
+            <input
+              className="source-input"
               type="text"
               value={this.state.comment}
               onChange={this.onInputComment}
             />
           </div>
           <div className="form-group">
-            <input className="button"
-              type="submit"
-              value="Submit"
-            />
+            <input className="button" type="submit" value="Submit" />
           </div>
         </form>
       </div>
