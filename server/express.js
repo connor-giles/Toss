142a8786c023aae1dce561e2521821f417a745f5
Connor Giles <39887479+connor-giles@users.noreply.github.com>
2020-12-07 20:43:34 -0500

    removed engine

--- server/express.js
+++ server/express.js
@@ -55,15 +55,22 @@ module.exports.init = () => {
 
   app.use(globalErrorHandler);
 
-  if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, '../../client/build')));
+  // if (process.env.NODE_ENV === 'production') {
+  //   // Serve any static files
+  //   app.use(express.static(path.join(__dirname, '../../client/build')));
 
-    // Handle React routing, return all requests to React app
-    app.get('*', function (req, res) {
-      res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
-    });
-  }
+  //   // Handle React routing, return all requests to React app
+  //   app.get('*', function (req, res) {
+  //     res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  //   });
+  // }
+
+  app.use('/', express.static('../client/build'));
+  app.use(express.static('../client/build'));
+  app.all('/*', (req, res) => {
+    // res.status(201).json({message: "nothing here!"});
+    res.sendFile(path.resolve("../client/build/index.html"));
+  });
 
   return app;
 };
