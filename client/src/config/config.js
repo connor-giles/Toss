952c7ab3ec9904bd058263afd168b5c6cee7a77b
Connor Giles <39887479+connor-giles@users.noreply.github.com>
2020-12-07 21:26:57 -0500

    changed url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     DOMAIN: {
       //name: "http://localhost:3001/",
-      name: "https://toss-cen.herokuapp.com/",
+      name: "https://pacific-sierra-34501.herokuapp.com/",
       frontendHome: "http://localhost:3000/"
     },
   };
