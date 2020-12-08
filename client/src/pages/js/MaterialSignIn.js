037a84543cda2355ce78fb0a29a5769c27eb0049
Anna Le <annale1501@gmail.com>
2020-12-08 15:24:24 -0500

    new formatting

--- client/src/pages/js/MaterialSignIn.js
+++ client/src/pages/js/MaterialSignIn.js
@@ -27,7 +27,7 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.primary.main,
+    backgroundColor: "#7C84FF",
   },
   form: {
     width: '100%', // Fix IE 11 issue.
@@ -35,6 +35,8 @@ const useStyles = makeStyles((theme) => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
+    background: "#7C84FF",
+    borderRadius: 50
   },
 }));
 
