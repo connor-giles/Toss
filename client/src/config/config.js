18d3062f6c00440dc53a0a172e48aacc8104cb37
Connor Giles <39887479+connor-giles@users.noreply.github.com>
2020-12-07 23:07:40 -0500

    heroku path test fix

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   DOMAIN: {
     //name: 'http://localhost:3001/',
-    name: "https://toss-cen.herokuapp.com/",
+    //name: "https://toss-cen.herokuapp.com/",
+    name: "https://pacific-sierra-34501.herokuapp.com/",
     frontendHome: 'http://localhost:3000/',
   },
 };
