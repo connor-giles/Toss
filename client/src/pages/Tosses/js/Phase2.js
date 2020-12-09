c641de72788da2a04b000d6b61f45c464cfd4f61
Anna Le <annale1501@gmail.com>
2020-12-09 14:33:20 -0500

    phase 2 format

--- client/src/pages/Tosses/js/Phase2.js
+++ client/src/pages/Tosses/js/Phase2.js
@@ -34,12 +34,70 @@ export default class SubmitResponse extends Component {
             />
           </div>
           <div className="form-group">
-            <input className="button" type="submit" value="1" />
-            <input className="button" type="submit" value="2" />
-            <input className="button" type="submit" value="3" />
-            <input className="button" type="submit" value="4" />
-            <input className="button" type="submit" value="5" />
-            <input className="button" type="submit" value="6" />
+            <input className="button" type="submit" value="fresh" />
+            <input className="button" type="submit" value="controversial" />
+            <input className="button" type="submit" value="objectivity" />
+            <input className="button" type="submit" value="credible" />
+            <input className="button" type="submit" value="constructive" />
+            <input className="button" type="submit" value="emotive" />
+          </div>
+        </form>
+        <form className="wrapper" onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <div className="prompt-wrapper">
+              <p className="prompt">Testing</p>
+            </div>
+            <label className="label"> Input Comment </label>
+            <textarea
+              className="comment-input"
+              type="text"
+              maxLength="500"
+            />
+          
+          </div>
+          <div className="form-group">
+            <label className="label"> Input Source </label>
+            <input
+              className="source-input"
+              type="text"
+            />
+          </div>
+          <div className="form-group">
+            <input className="button" type="submit" value="fresh" />
+            <input className="button" type="submit" value="controversial" />
+            <input className="button" type="submit" value="objectivity" />
+            <input className="button" type="submit" value="credible" />
+            <input className="button" type="submit" value="constructive" />
+            <input className="button" type="submit" value="emotive" />
+          </div>
+        </form>
+        <form className="wrapper" onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <div className="prompt-wrapper">
+              <p className="prompt">Testing</p>
+            </div>
+            <label className="label"> Input Comment </label>
+            <textarea
+              className="comment-input"
+              type="text"
+              maxLength="500"
+            />
+          
+          </div>
+          <div className="form-group">
+            <label className="label"> Input Source </label>
+            <input
+              className="source-input"
+              type="text"
+            />
+          </div>
+          <div className="form-group">
+            <input className="button" type="submit" value="fresh" />
+            <input className="button" type="submit" value="controversial" />
+            <input className="button" type="submit" value="objectivity" />
+            <input className="button" type="submit" value="credible" />
+            <input className="button" type="submit" value="constructive" />
+            <input className="button" type="submit" value="emotive" />
           </div>
         </form>
       </div>
